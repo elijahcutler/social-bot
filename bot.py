8aa46f1e6fd996c1928ca991499f484bd6f82aba
Elijah Cutler <ecutler@cykor.com>
2024-10-28 21:03:48 -0400

    updated script

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import discord
-from discord.ext import commands
+
 import subprocess
 import os
 
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = discord.Bot()
 
 container_name = os.getenv('DOCKER_CONTAINER_NAME')
 
@@ -12,26 +11,28 @@ if not container_name:
     print('Error: DOCKER_CONTAINER_NAME environment variable not set.')
     exit(1)
 
-@bot.slash_command(name='start_server', description='Start the Minecraft server.')
-async def start_server(ctx):
+server = bot.create_group("server", "Manage server")
+
+@server.command(name='start', description='Start the Minecraft server.')
+async def start(ctx):
     await ctx.respond('Starting the Minecraft server...')
     subprocess.run(['docker', 'start', container_name])
     await ctx.send('Minecraft server started.')
 
-@bot.slash_command(name='stop_server', description='Stop the Minecraft server.')
-async def stop_server(ctx):
+@server.command(name='stop', description='Stop the Minecraft server.')
+async def stop(ctx):
     await ctx.respond('Stopping the Minecraft server...')
     subprocess.run(['docker', 'stop', container_name])
     await ctx.send('Minecraft server stopped.')
 
-@bot.slash_command(name='restart_server', description='Restart the Minecraft server.')
-async def restart_server(ctx):
+@server.command(name='restart', description='Restart the Minecraft server.')
+async def restart(ctx):
     await ctx.respond('Restarting the Minecraft server...')
     subprocess.run(['docker', 'restart', container_name])
     await ctx.send('Minecraft server restarted.')
 
-@bot.slash_command(name='server_status', description='Get the status of the Minecraft server.')
-async def server_status(ctx):
+@server.command(name='status', description='Get the status of the Minecraft server.')
+async def status(ctx):
     result = subprocess.run([f'docker', 'ps', '--filter', f'name={container_name}'], stdout=subprocess.PIPE)
     if container_name.encode() in result.stdout:
         await ctx.respond(f'Minecraft server ({container_name}) is running.')
